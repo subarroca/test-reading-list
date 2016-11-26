01847a692dbffd7cf05ba2fa3315d27dcc9e4733
Salvador Subarroca <subarroca@gmail.com>
2016-11-26 20:19:13 +0100

    chore: tweak some initial configs (sass, component prefix)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'kw-app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'app works!';
